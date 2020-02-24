19202c07d3b522b5acb19a2745c755211b8cc65c
Lucas Baggio Figueira <lucasbf@gmail.com>
2020-02-24 19:15:02 +0000

    Configuração para compilar C++ no Gitpod

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string nome;
+
+    cout << "Qual é o seu nome? " << endl;
+    cin >> nome;
+    cout << "Olá " << nome << ", como vai você!" << endl;
+
+    return 0;
+}
